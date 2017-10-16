900649621f6f124ace7ec1ed55c8be9e2fb4ff0e
abbydarda <darda.abby@gmail.com>
2017-10-16 16:28:20 +0700

    tryout

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ecl2/ci_controller+view+model/';
+$config['base_url'] = 'http://localhost/tryout/';
 
 /*
 |--------------------------------------------------------------------------
