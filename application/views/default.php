ef7a63aa22e508e6cf0ab1d09be4eab76cee464e
abbydarda <darda.abby@gmail.com>
2017-10-16 13:40:38 +0700

    tryout

---
+++ application/views/default.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit("no direct access script allowed");?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $title; ?></title>
+</head>
+<body>
+	<h1><?php echo $title; ?></h1>
+	<em>Penulis Adalah : <?php echo $author; ?></em><br>
+	<em>Pada Tanggal : <?php echo $date; ?></em><br>
+	<em>Isi Artikel adalah : <?php echo $content; ?></em>
+</body>
+</html>
