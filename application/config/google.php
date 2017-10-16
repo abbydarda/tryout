5875cb0245e9e5ae941842d80a2c7b201ea9111b
abbydarda <darda.abby@gmail.com>
2017-10-16 16:35:23 +0700

    tryout

---
+++ application/config/google.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------
+|  Google API Configuration
+| -------------------------------------------------------------------
+|  client_id         string   Your Google API Client ID.
+|  client_secret     string   Your Google API Client secret.
+|  redirect_uri      string   URL to redirect back to after login.
+|  application_name  string   Your Google application name.
+|  api_key           string   Developer key.
+|  scopes            string   Specify scopes
+*/
+$config['google']['client_id']        = '120787494571-uvbuv7pnark0kphkelhok8snq6k2kp7h.apps.googleusercontent.com';
+$config['google']['client_secret']    = 'ZHH-MQ4gwKSRF46e2PowEdXN';
+$config['google']['redirect_uri']     = 'http://localhost/tryout/';
+$config['google']['application_name'] = 'Login';
+$config['google']['api_key']          = '';
+$config['google']['scopes']           = array();
+?>
---
+++ application/views/Login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Login</title>
+		<link rel="stylesheet" href="<?php echo site_url();?>assets/css/style.css">
+	</head>
+	<body>
+		<div class="wrapper">
+			<form class="form-signin">
+				<h2 class="form-signin-heading">Please login</h2>
+				<a href="<?php echo base_url();?>index.php/Google_auth"><img src="<?php echo base_url().'assets/google.png'; ?>" heigth="50px" width="50px"/></a>
+			</form>
+		</div>
+	</body>
+</html>
